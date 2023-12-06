b2cc12b33838a31f13d3953e1fdd60bf9149848a
Precious Nnam <nnamvannessa@gmail.com>
2023-12-06 19:26:17 +0100

    react app setup, navigation component with styling

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "./components/navigation/navigation";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <div className="container" id="container">
+        <Navigation />
+      </div>
+    </div>
+  );
+};
+// nn
+export default App;
---
+++ src/components/navigation/navigation.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import "./navigation.css";
+import Logo from "../../assets/mainstack-logo.png";
+import { GoHome } from "react-icons/go";
+import {
+  MdOutlineInsertChartOutlined,
+  MdOutlineWidgets,
+  MdOutlinePayments,
+  MdOutlineGroup,
+  MdNotificationsNone,
+  MdMenu,
+  MdOutlineChat,
+} from "react-icons/md";
+
+const Navigation: React.FC = () => {
+  return (
+    <nav className="nav">
+      <div className="nav-logo">
+        <span>
+          <img src={Logo} alt="mainstack-logo" />
+        </span>
+      </div>
+      <div className="nav-menu">
+        <div>
+          <span>
+            <GoHome className="icon" />
+          </span>
+          Home
+        </div>
+        <div>
+          <span>
+            <MdOutlineInsertChartOutlined className="icon" />
+          </span>
+          Analytics
+        </div>
+        <div className="active">
+          <span>
+            <MdOutlinePayments className="icon" />
+          </span>
+          Revenue
+        </div>
+        <div>
+          <span>
+            <MdOutlineGroup className="icon" />
+          </span>
+          CRM
+        </div>
+        <div>
+          <span>
+            <MdOutlineWidgets className="icon" />
+          </span>
+          Apps
+        </div>
+      </div>
+      <div className="nav-panel">
+        <div>
+          <span>
+            <MdNotificationsNone className="icon" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <MdOutlineChat className="icon" />
+          </span>
+        </div>
+        <div>
+          <div>OJ</div>
+          <span>
+            <MdMenu className="icon" />
+          </span>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
